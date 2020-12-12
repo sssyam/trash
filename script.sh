05e24e6cd79996112363679bce064318877374b5
Satyam Sinha <satyamsinha19@gmail.com>
2020-12-12 13:19:01 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+sudo yum install git -y
+/bin/bash -c "$(curl -fsSL https://github.com/gmurtaza24/SAMInstallation/blob/main/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile
+echo "eval $($(brew --prefix)/bin/brew shellenv)" >> ~/.bash_profile
+echo "eval $($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+brew tap aws/tap
+brew install aws-sam-cli
